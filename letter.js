079adb31f2a3d5d4f8a734f89fa0e095fd5aa59a
powhd <1040999649@qq.com>
2017-08-23 17:05:17 +0800

    Add files via upload

---
+++ letter.js
@@ -0,0 +1,23 @@
+﻿let display_time = 100;
+(function($) {
+	$.fn.write = function() {
+		this.each(function() {
+			var $el = $(this), str = $el.html(), cur_idx = 0;
+			var timer = setInterval(function() {
+				var current = str.substr(cur_idx, 1);
+				if (current == '<') {
+					cur_idx = str.indexOf('>', cur_idx) + 1;
+				} else {
+					cur_idx++;
+				}
+				$el.html(str.substring(0, cur_idx) + (cur_idx & 1 ? '_' : ''));
+				if (cur_idx >= str.length) {
+					clearInterval(timer);
+				}
+			}, display_time);
+		});
+		return this;
+	};
+})($);
+
+
